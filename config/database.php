33c2e7eff04463a9995d82765688fd8b3e241e2b
Isaias <chaisystem@gmail.com>
2022-06-21 14:30:25 -0500

    migrate
    php artisan optimize:clear
    
    php artisan migrate
    
    y si falla migrations modificar database.php

--- config/database.php
+++ config/database.php
@@ -52,12 +52,12 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8mb4', /*quitar mb4*/
+            'collation' => 'utf8mb4_unicode_ci', /*quitar mb4*/
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => null, /*innodb*/
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
