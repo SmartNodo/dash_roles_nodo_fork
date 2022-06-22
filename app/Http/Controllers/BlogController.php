a6cf8706c89af3bece2b9515413d76a72ba6b9de
Isaias <chaisystem@gmail.com>
2022-06-22 00:38:15 -0500

    Blog Controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,82 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
+
 
 class BlogController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
+    function __construct(){
+        $this->middleware('permission:ver-blog | crear-blog | editar-blog | borrar-blog')->only('index');
+        $this->middleware('permission:crear-blog', ['only'=>['create','store']]);
+        $this->middleware('permission:editar-blog', ['only'=>['edit','update']]);
+        $this->middleware('permission:borrar-blog', ['only'=>['destroy']]);
+        
+    }
     public function index()
     {
-        //
+        $blogs = Blog::paginate(5);
+        return view('blogs.index', compact('blogs'));  
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('blogs.crear');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+                'titulo' => 'required', 
+                'contenido'=>'required'
+            ]
+        );
+
+        Role::create($request->all());
+
+        return redirect()->route('blogs.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function edit(Blog $blog)
     {
-        //
+        return view('blogs.editar', compact('blog'));        
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function update(Request $request, Blog $blog)
     {
-        //
+        $this->validate($request, [
+                'titulo' => 'required', 
+                'contenido'=>'required'
+            ]
+        );
+
+        $blog->update($request->all());
+
+        return redirect()->route('blogs.index');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function destroy(Blog $blog)
     {
-        //
-    }
+        $blog->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return redirect()->route('blogs.index');
     }
 }
