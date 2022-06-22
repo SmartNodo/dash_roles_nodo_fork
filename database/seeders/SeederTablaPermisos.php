c31a6f1212f3e5346ce5f892dbeefa5249ad2a2d
Isaias <chaisystem@gmail.com>
2022-06-22 14:38:36 -0500

    Crear seed y ejecutar
    php artisan db:seed --class=SeederTablaPermisos

--- database/seeders/SeederTablaPermisos.php
+++ database/seeders/SeederTablaPermisos.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use Spatie\Permission\Models\Permission;
+
 class SeederTablaPermisos extends Seeder
 {
     /**
@@ -13,6 +15,21 @@ class SeederTablaPermisos extends Seeder
      */
     public function run()
     {
-        //
+        $permisos = [
+            // roles
+            'ver-rol',
+            'crear-rol',
+            'editar-rol',
+            'borrar-rol',
+            // blogs
+            'ver-blog',
+            'crear-blog',
+            'editar-blog',
+            'borrar-blog',
+        ];
+
+        foreach($permisos as $permiso){
+            Permission::create(['name'=>$permiso]);
+        }
     }
 }
