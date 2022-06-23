f138a140ccc977c91177c0c7a1c1c808246c9ce3
Isaias <chaisystem@gmail.com>
2022-06-22 20:51:25 -0500

    blog view crud

---
+++ resources/views/blogs/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h3 class="page__heading">Blogs</h3>
+        </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+
+                            @can('crear-blog')
+                                <a class="btn btn-warning" href="{{ route('blogs.create') }}"> Nuevo</a>
+                            @endcan
+
+                            <table class="table table-triped mt-2">
+                                <thead>
+                                    <th style="display: none">ID</th>
+                                    <th>Titulo</th>
+                                    <th>Contenido</th>                                    
+                                    <th>Acciones</th>
+                                </thead>
+                                <tbody>
+                                    @foreach ($blogs as $blog)
+                                        <tr>
+                                            <td style="display: none">{{ $blog->id}}</td>
+                                            <td>{{ $blog->titulo}}</td>
+                                            <td>{{ $blog->contenido}}</td>
+                                            <td>
+
+                                                @can('editar-blog')                                                    
+                                                    <a class="btn btn-info" href="{{route('blogs.edit', $blog->id)}}">Editar</a>
+                                                @endcan
+
+                                                @can('editar-blog')                                                    
+                                                {!! Form::open(['method'=> 'DELETE', 'route'=> ['blogs.destroy', $blog->id], 'style'=>'display:inline']) !!}
+                                                    {!! Form::submit('Borrar', ['class'=>'btn btn-danger']) !!}
+                                                {!! Form::close() !!}
+                                                @endcan
+                                                
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            <div class="pagination justify-content-end">
+                                {!! $blogs->links() !!}
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
