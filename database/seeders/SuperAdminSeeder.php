29a889d785a0ef85e3831c4c8596955a4ef15e98
Isaias <chaisystem@gmail.com>
2022-06-22 23:13:17 -0500

    Seed superadmin
    php artisan make:seeder SuperAdminSeeder
    
    php artisan db:seed --class SuperAdminSeeder

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Role;
+
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $usuario = User::create([
+            'name' => 'Isaias Soto',
+            'email' => 'isoto@gmail.com',
+            'password' => bcrypt('123456789')
+        ]);
+
+        //Para cuando no se ha creador el rol Administrador
+        // $rol = Role::create(['name'=>'Administrador']);
+        // $permisos = Permission::pluck('id','id')->all();
+        // $rol->syncPermissions($permisos);
+        // $usuario->assignRole([$rol->id]);
+
+        // Cuando ya tenemos el rol Administrador
+        // $rol = Role::create(['name'=>'Administrador']);
+        // $permisos = Permission::pluck('id','id')->all();
+        // $rol->syncPermissions($permisos);
+        $usuario->assignRole('Administrador');
+
+    }
+}
