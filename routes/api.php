ad724ec0c12015977be5e8feb9f67d7600863ab1
Isaias <chaisystem@gmail.com>
2022-06-23 16:12:10 -0500

    api route queve

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\UserController;
 
+use App\Models\Queve;
+
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 
@@ -12,6 +14,11 @@ Route::group( ['middleware' => ["auth:sanctum"]], function(){
     //rutas
     Route::get('user-profile', [UserController::class, 'userProfile']);
     Route::get('logout', [UserController::class, 'logout']);
+
+    //rutas para Queve    
+    Route::post("create-queve", [QueveController::class, "createQueve"]); // crear un blog
+    Route::get("list-queve", [QueveController::class, "listQueve"]); //mostrar TODOS los blogs
+    Route::get("show-queve/{id}", [QueveController::class, "showQueve"]); //mostrar UN solo blog
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
