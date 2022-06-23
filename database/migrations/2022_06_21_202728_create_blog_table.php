03983dd91706a26d4053aea9fe13d6fe471e8fe2
Isaias <chaisystem@gmail.com>
2022-06-22 20:50:55 -0500

    fix migrations blogs

--- database/migrations/2022_06_21_202728_create_blog_table.php
+++ database/migrations/2022_06_21_202728_create_blog_table.php
@@ -13,7 +13,7 @@ class CreateBlogTable extends Migration
      */
     public function up()
     {
-        Schema::create('blog', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
             $table->text('contenido');
@@ -28,6 +28,6 @@ class CreateBlogTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blog');
+        Schema::dropIfExists('blogs');
     }
 }
