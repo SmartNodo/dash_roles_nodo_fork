e957d8afd4d8a263c07501dde107ad2ea24e9f69
Carlos Hernández <carlos_develops@outlook.com>
2022-10-04 16:24:18 -0500

    mensaje de validación de formulario

--- resources/views/credit-consulter/index.blade.php
+++ resources/views/credit-consulter/index.blade.php
@@ -8,6 +8,7 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link href="{{ asset('assets/css/sweetalert.css') }}" rel="stylesheet" type="text/css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script> --}}
 
@@ -120,7 +121,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-
+    <script src="{{ asset('assets/js/sweetalert.min.js') }}"></script>
     <script>
         let creditNumber, idState;
 
@@ -135,6 +136,9 @@
 
             // Validación:
             if(!idState || !creditNumber) {
+
+                swal("Los campos son obligatorios", "", "info");
+
                 return 0
             }
 
